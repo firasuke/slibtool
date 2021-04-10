1bb591b6b6c9d4b2d4933de7960bcf4f0aa3e1ba
midipix <writeonce@midipix.org>
2021-04-10 10:27:33 +0000

    project: posix make support: refactored the cflag definition of SLBT_MACHINE.

--- project/extras.mk
+++ project/extras.mk
@@ -2,13 +2,11 @@ CFLAGS_SHARED_ATTR	+= -DSLBT_PRE_ALPHA -DSLBT_EXPORT
 CFLAGS_STATIC_ATTR	+= -DSLBT_PRE_ALPHA -DSLBT_STATIC
 CFLAGS_APP_ATTR		+= -DSLBT_APP
 
-CFLAGS_MACHINE		:= -DSLBT_MACHINE=\"$(shell $(CC) $(CFLAGS) -dumpmachine)\"
+CFLAGS_MACHINE		:= -DSLBT_MACHINE=\"$(CC_HOST)\"
+CFLAGS_ONCE		+= $(CFLAGS_MACHINE)
 
 src/driver/slbt_driver_ctx.o:	version.tag
 src/driver/slbt_driver_ctx.lo:	version.tag
-
-src/driver/slbt_driver_ctx.o:	CFLAGS += $(CFLAGS_MACHINE)
-src/driver/slbt_driver_ctx.lo:	CFLAGS += $(CFLAGS_MACHINE)
 
 install-app-extras:	DBGNAME  = dlibtool
 install-app-extras:	LEGABITS = clibtool
