8a9ab12c467fe48944272ae9bd073f90854b13f8
midipix <writeonce@midipix.org>
2024-01-01 15:13:57 +0000

    project: install [s|d|c]libtool-ar symlinks for automatic ar mode detection.

--- project/extras.mk
+++ project/extras.mk
@@ -20,14 +20,17 @@ install-app-extras:
 
 	rm -f bin/$(NICKNAME)-shared$(OS_APP_SUFFIX).tmp
 	rm -f bin/$(NICKNAME)-static$(OS_APP_SUFFIX).tmp
+	rm -f bin/$(NICKNAME)-ar$(OS_APP_SUFFIX).tmp
 
 	rm -f bin/$(DBGNAME)$(OS_APP_SUFFIX).tmp
 	rm -f bin/$(DBGNAME)-shared$(OS_APP_SUFFIX).tmp
 	rm -f bin/$(DBGNAME)-static$(OS_APP_SUFFIX).tmp
+	rm -f bin/$(DBGNAME)-ar$(OS_APP_SUFFIX).tmp
 
 	rm -f bin/$(LEGABITS)$(OS_APP_SUFFIX).tmp
 	rm -f bin/$(LEGABITS)-shared$(OS_APP_SUFFIX).tmp
 	rm -f bin/$(LEGABITS)-static$(OS_APP_SUFFIX).tmp
+	rm -f bin/$(LEGABITS)-ar$(OS_APP_SUFFIX).tmp
 
 	rm -f bin/$(RAPUNZEL)$(OS_APP_SUFFIX).tmp
 	rm -f bin/$(RAPUNCEL)$(OS_APP_SUFFIX).tmp
@@ -36,14 +39,17 @@ install-app-extras:
 
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(NICKNAME)-shared$(OS_APP_SUFFIX).tmp
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(NICKNAME)-static$(OS_APP_SUFFIX).tmp
+	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(NICKNAME)-ar$(OS_APP_SUFFIX).tmp
 
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(DBGNAME)$(OS_APP_SUFFIX).tmp
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(DBGNAME)-shared$(OS_APP_SUFFIX).tmp
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(DBGNAME)-static$(OS_APP_SUFFIX).tmp
+	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(DBGNAME)-ar$(OS_APP_SUFFIX).tmp
 
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(LEGABITS)$(OS_APP_SUFFIX).tmp
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(LEGABITS)-shared$(OS_APP_SUFFIX).tmp
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(LEGABITS)-static$(OS_APP_SUFFIX).tmp
+	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(LEGABITS)-ar$(OS_APP_SUFFIX).tmp
 
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(RAPUNZEL)$(OS_APP_SUFFIX).tmp
 	ln -s ./$(NICKNAME)$(OS_APP_SUFFIX) bin/$(RAPUNCEL)$(OS_APP_SUFFIX).tmp
@@ -52,14 +58,17 @@ install-app-extras:
 
 	mv bin/$(NICKNAME)-shared$(OS_APP_SUFFIX).tmp $(DESTDIR)$(BINDIR)/$(NICKNAME)-shared$(OS_APP_SUFFIX)
 	mv bin/$(NICKNAME)-static$(OS_APP_SUFFIX).tmp $(DESTDIR)$(BINDIR)/$(NICKNAME)-static$(OS_APP_SUFFIX)
+	mv bin/$(NICKNAME)-ar$(OS_APP_SUFFIX).tmp     $(DESTDIR)$(BINDIR)/$(NICKNAME)-ar$(OS_APP_SUFFIX)
 
 	mv bin/$(DBGNAME)$(OS_APP_SUFFIX).tmp         $(DESTDIR)$(BINDIR)/$(DBGNAME)$(OS_APP_SUFFIX)
 	mv bin/$(DBGNAME)-shared$(OS_APP_SUFFIX).tmp  $(DESTDIR)$(BINDIR)/$(DBGNAME)-shared$(OS_APP_SUFFIX)
 	mv bin/$(DBGNAME)-static$(OS_APP_SUFFIX).tmp  $(DESTDIR)$(BINDIR)/$(DBGNAME)-static$(OS_APP_SUFFIX)
+	mv bin/$(DBGNAME)-ar$(OS_APP_SUFFIX).tmp      $(DESTDIR)$(BINDIR)/$(DBGNAME)-ar$(OS_APP_SUFFIX)
 
 	mv bin/$(LEGABITS)$(OS_APP_SUFFIX).tmp        $(DESTDIR)$(BINDIR)/$(LEGABITS)$(OS_APP_SUFFIX)
 	mv bin/$(LEGABITS)-shared$(OS_APP_SUFFIX).tmp $(DESTDIR)$(BINDIR)/$(LEGABITS)-shared$(OS_APP_SUFFIX)
 	mv bin/$(LEGABITS)-static$(OS_APP_SUFFIX).tmp $(DESTDIR)$(BINDIR)/$(LEGABITS)-static$(OS_APP_SUFFIX)
+	mv bin/$(LEGABITS)-ar$(OS_APP_SUFFIX).tmp     $(DESTDIR)$(BINDIR)/$(LEGABITS)-ar$(OS_APP_SUFFIX)
 
 	mv bin/$(RAPUNZEL)$(OS_APP_SUFFIX).tmp        $(DESTDIR)$(BINDIR)/$(RAPUNZEL)$(OS_APP_SUFFIX)
 	mv bin/$(RAPUNCEL)$(OS_APP_SUFFIX).tmp        $(DESTDIR)$(BINDIR)/$(RAPUNCEL)$(OS_APP_SUFFIX)
